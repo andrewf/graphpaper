7fb6ddea513ab6e8de169078ee44e601d5dd39ac
Andrew Fleenor <andrewflnr@gmail.com>
2011-12-11 18:50:34 -0800

    something resembling a running unit-test framework.

--- premake4.lua
+++ premake4.lua
@@ -1,11 +1,21 @@
+if _ACTION == "clean" then
+    -- clean up manually generated files
+    os.execute "rm -f sample.database"
+    os.execute "rm -f src/tests/AllTests.c"
+else
+    -- manually generate some files
+    os.execute "sqlite3 sample.database < sampledata.sql"
+    os.execute "cd src/graphpaper && ../cutest/make-tests.sh > ../test/AllTests.c"
+end
+
 solution "graphpaper"
     language "C"
     configurations {"release", "debug"}
-    project "libgraphpaper"
-        files "*.c"
-        excludes "test.c"
+    project "graphpaper"
+        files "src/graphpaper/**.c"
         kind "StaticLib"
     project "graphtest"
-        files "test.c"
+        files {"src/cutest/CuTest.c", "src/test/**.c"}
+        includedirs "src/cutest"
         kind "ConsoleApp"
-        links "libgraphpaper"
+        links "graphpaper"
