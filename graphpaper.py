200c104c437bb63af6f02bb579fd0bcaa8518098
Andrew Fleenor <andrewflnr@gmail.com>
2012-01-05 13:34:58 -0800

    Allow resizing cards
    I had to wrap the text widget in a frame with a fat border,
    manually catch mouse events and handle resizing and moving. I
    also had to violate the private interface of model.Card to save
    the result efficiently. It's not beautiful, but it works.

--- graphpaper.py
+++ graphpaper.py
@@ -20,16 +20,26 @@ class ViewportCard(object):
         self.draw()
         self.moving = False
         self.editing = False
+        self.resize_state = None
     def draw(self):
-        self.window = Text(self.canvas)
-        self.window.bind("<Button-1>", self.mousedown)
-        self.window.bind("<Shift-Button-1>", self.shiftmousedown)
-        self.window.bind("<B1-Motion>", self.mousemove)
-        self.window.bind("<ButtonRelease-1>", self.mouseup)
-        self.window.bind("<FocusIn>", self.focusin)
-        self.window.bind("<FocusOut>", self.focusout)
-        self.window.bind("<Control-Delete>", self.ctrldelete)
-        self.window.insert(END, self.card.text)
+        self.frame_thickness = 5
+        self.window = Frame(self.canvas, borderwidth=self.frame_thickness, cursor='cross')
+        self.text = Text(self.window)
+        self.text.pack(expand=1, fill='both')
+        # set up text for editing, dragging, deleting
+        self.text.bind("<Button-1>", self.mousedown)
+        self.text.bind("<Shift-Button-1>", self.shiftmousedown)
+        self.text.bind("<B1-Motion>", self.mousemove)
+        self.text.bind("<ButtonRelease-1>", self.mouseup)
+        self.text.bind("<FocusIn>", self.focusin)
+        self.text.bind("<FocusOut>", self.focusout)
+        self.text.bind("<Control-Delete>", self.ctrldelete)
+        self.text.insert(END, self.card.text)
+        # set up frame for resizing
+        self.window.bind("<Button-1>", self.frame_mousedown)
+        self.window.bind("<B1-Motion>", self.frame_mousemove)
+        self.window.bind("<ButtonRelease-1>", self.frame_mouseup)
+        # add item to canvas
         self.itemid = self.canvas.create_window(
             self.card.x,
             self.card.y,
@@ -41,12 +51,14 @@ class ViewportCard(object):
         )
     def save_text(self):
         # get text from window
-        text = self.window.get('0.0', END)
+        text = self.text.get('0.0', END)
         if text != self.card.text:
             print 'new card text: "%s"' % text
             self.card.text = text
         else:
             print 'card unchanged'
+    def canvas_coords(self):
+        return map(int, self.canvas.coords(self.itemid))
     def mousedown(self, event):
         self.window.lift()
     def shiftmousedown(self, event):
@@ -65,7 +77,7 @@ class ViewportCard(object):
     def mouseup(self, event):
         if self.moving:
             self.moving = False
-            new_coords = map(int, self.canvas.coords(self.itemid))
+            new_coords = self.canvas_coords()
             self.card.set_pos(new_coords[0], new_coords[1])
     def focusin(self, event):
         print "focusing text"
@@ -80,6 +92,54 @@ class ViewportCard(object):
             self.card.delete()
             self.canvas.delete(self.itemid)
             self.window.destroy()
+    def frame_mousedown(self, event):
+        # store initial coords and which edges are 
+        self.resize_state = {
+            'start_coords': (event.x, event.y),
+            'last_coords': (event.x, event.y),
+            'left_edge': (0 <= event.x <= self.frame_thickness),
+            'right_edge': (event.x >= self.window.winfo_width() - self.frame_thickness),
+            'top_edge': (0 < event.y < self.frame_thickness),
+            'bottom_edge': (event.y > self.window.winfo_height() - self.frame_thickness)
+        }
+    def frame_mousemove(self, event):
+        if self.resize_state:
+            resize = self.resize_state # debug var
+            event_x = event.x
+            event_y = event.y
+            # distance of cursor from original position of window
+            delta = map(int, (event.x - self.resize_state['start_coords'][0],
+                              event.y - self.resize_state['start_coords'][1]))
+            # load current pos, size
+            new_x, new_y = self.canvas_coords()
+            new_width = int(self.canvas.itemcget(self.itemid, 'width'))
+            new_height = int(self.canvas.itemcget(self.itemid, 'height'))
+            # handle x resize/move
+            if self.resize_state['left_edge']:
+                # must move pos and resize
+                new_x += delta[0]
+                new_width -= delta[0]
+            elif self.resize_state['right_edge']:
+                new_width += (event.x - self.resize_state['last_coords'][0])
+            # handle y resize/move
+            if self.resize_state['top_edge']:
+                new_y += delta[1]
+                new_height -= delta[1]
+            elif self.resize_state['bottom_edge']:
+                new_height += (event.y - self.resize_state['last_coords'][1])
+            # save new settings in item, not card yet
+            self.resize_state['last_coords'] = (event.x, event.y)
+            self.canvas.coords(self.itemid, new_x, new_y)
+            self.canvas.itemconfig(self.itemid, width=new_width, height=new_height)
+    def frame_mouseup(self, event):
+        if self.resize_state:
+            self.card._x, self.card._y = self.canvas_coords()
+            self.card._w = int(self.canvas.itemcget(self.itemid, 'width'))
+            self.card._h = int(self.canvas.itemcget(self.itemid, 'height'))
+            print 'saving:', self.card.x, self.card.y, self.card.w, self.card.h
+            self.card.save()
+            print 'deleting resize state'
+            self.resize_state = None
  
 
 class GPViewport(Frame):
@@ -153,6 +213,13 @@ class GPViewport(Frame):
             self.reset_scroll_region()
         button = Button(self.util, text="moveit", command=move_card)
         button.pack()
+        def resize_card():
+            card = self.cards[0]
+            card.card.w += 10
+            card.card.h += 10
+            self.canvas.itemconfig(card.itemid, width=card.card.w, height=card.card.h)
+        Button(self.util, text="resize", command=resize_card).pack()
+
     def new_card(self, x, y, w, h):
         self.cards.append(ViewportCard(self, self.data.new_card(x, y, w, h)))
     def save_scroll_pos(self):
