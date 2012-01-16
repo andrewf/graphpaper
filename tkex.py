88555c487b873c7de4b3e4130c9b2b96dae2f613
Andrew Fleenor <andrewflnr@gmail.com>
2012-01-15 23:39:07 -0800

    Refactor resizing functionality into separate class

---
+++ tkex.py
@@ -0,0 +1,102 @@
+'''
+Extra helpful widgets for Tkinter.
+'''
+
+from Tkinter import *
+
+class ResizableCanvasFrame(Frame):
+    '''
+    Class that handles creating resizable frames on a canvas.
+    Don't pack it.
+
+    Set save_callback to whatever you want to happen when the mouse
+    lets up on the border.
+    '''
+    def __init__(self, master, x, y, w, h, *args, **kwargs):
+        # master should be a Canvas
+        self.frame_thickness = 5
+        Frame.__init__(
+            self,
+            master,
+            *args,
+            borderwidth = self.frame_thickness,
+            cursor = 'fleur',
+            **kwargs
+        )
+        self.canvas = master
+        self.resize_state = None
+        self.bind('<Button-1>', self.mousedown)
+        self.bind('<B1-Motion>', self.mousemove)
+        self.bind('<ButtonRelease-1>', self.mouseup)
+        self.bind('<Destroy>', self.delete_item)
+        # add self to canvas
+        self.itemid = self.canvas.create_window(
+            x,
+            y,
+            window=self,
+            anchor="nw",
+            width=w,
+            height=h
+        )
+        self.save_callback = None
+        
+    def canvas_coords(self):
+        "Window position on canvas as a tuple of integers"
+        return map(int, self.canvas.coords(self.itemid))
+    
+    def move(self, dx, dy):
+        # strictly, this is out of the range of RCF,
+        # but it helps with the law of demeter
+        self.canvas.move(self.itemid, dx, dy)
+
+    def mousedown(self, event):
+        window_width = self.winfo_width()
+        window_height = self.winfo_height()
+        self.resize_state = {
+            'start_coords': (event.x, event.y),
+            'last_coords': (event.x, event.y),
+            'left_edge': (0 <= event.x < self.frame_thickness),
+            'right_edge': (window_width - self.frame_thickness <= event.x < window_width),
+            'top_edge': (0 <= event.y < self.frame_thickness),
+            'bottom_edge': (window_height - self.frame_thickness <= event.y < window_height),
+        }            
+
+    def mousemove(self, event):
+        if self.resize_state:
+            resize = self.resize_state # debug var
+            event_x = event.x
+            event_y = event.y
+            # distance of cursor from original position of window
+            delta = map(int, (event.x - self.resize_state['start_coords'][0],
+                              event.y - self.resize_state['start_coords'][1]))
+            # load current pos, size
+            new_x, new_y = self.canvas_coords()
+            new_width = int(self.canvas.itemcget(self.itemid, 'width'))
+            new_height = int(self.canvas.itemcget(self.itemid, 'height'))
+            # handle x resize/move
+            if self.resize_state['left_edge']:
+                # must move pos and resize
+                new_x += delta[0]
+                new_width -= delta[0]
+            elif self.resize_state['right_edge']:
+                new_width += (event.x - self.resize_state['last_coords'][0])
+            # handle y resize/move
+            if self.resize_state['top_edge']:
+                new_y += delta[1]
+                new_height -= delta[1]
+            elif self.resize_state['bottom_edge']:
+                new_height += (event.y - self.resize_state['last_coords'][1])
+            # save new settings in item, not card yet
+            self.resize_state['last_coords'] = (event.x, event.y)
+            self.canvas.coords(self.itemid, new_x, new_y)
+            self.canvas.itemconfig(self.itemid, width=new_width, height=new_height)
+
+    def mouseup(self, event):
+        if self.resize_state:
+            self.resize_state = None
+            if self.save_callback:
+                self.save_callback()
+
+    def delete_item(self, event):
+        self.canvas.delete(self.itemid)
+
--- viewportcard.py
+++ viewportcard.py
@@ -2,6 +2,8 @@ from Tkinter import *
 from ScrolledText import ScrolledText
 import tkMessageBox
 
+from tkex import ResizableCanvasFrame
+
 class ViewportCard(object):
     '''
     Manages the graphical representation of a card in a
@@ -21,7 +23,13 @@ class ViewportCard(object):
 
     def draw(self):
         self.frame_thickness = 5
-        self.window = Frame(self.canvas, borderwidth=self.frame_thickness, cursor='fleur')
+        self.window = ResizableCanvasFrame(
+            self.canvas,
+            self.card.x,
+            self.card.y,
+            self.card.w,
+            self.card.h,
+        )
         self.text = ScrolledText(self.window, wrap=WORD)
         self.text.pack(expand=1, fill='both')
         # set up text for editing, dragging, deleting
@@ -35,19 +43,8 @@ class ViewportCard(object):
         self.text.bind("<Control-Delete>", self.ctrldelete)
         self.text.insert(END, self.card.text)
         # set up frame for resizing
-        self.window.bind("<Button-1>", self.frame_mousedown)
-        self.window.bind("<B1-Motion>", self.frame_mousemove)
-        self.window.bind("<ButtonRelease-1>", self.frame_mouseup)
-        # add item to canvas
-        self.itemid = self.canvas.create_window(
-            self.card.x,
-            self.card.y,
-            window = self.window,
-            anchor = "nw",
-            width = self.card.w,
-            height = self.card.h,
-            tags = 'card'
-        )
+        #self.window.bind('<Configure>', self.configure)
+        self.window.save_callback = self.save_card
 
     def get_text(self):
         "gets the text from the actual editor, which may not be saved yet"
@@ -60,7 +57,7 @@ class ViewportCard(object):
             self.card.text = text
 
     def canvas_coords(self):
-        return map(int, self.canvas.coords(self.itemid))
+        return self.window.canvas_coords()
 
     def start_moving(self, event):
         # set up state for a drag
@@ -80,7 +77,7 @@ class ViewportCard(object):
         # move the opposite direction the viewport scrolled
         scroll_x, scroll_y = -scroll_x, -scroll_y
         #print 'card.edgescroll x y', scroll_x, scroll_y, 'relative_mouse_pos', relative_mouse_pos
-        self.canvas.move(self.itemid, scroll_x, scroll_y)
+        self.window.move(scroll_x, scroll_y)
         self.viewport.reset_scroll_region()
         self.set_moving_edgescroll_callback()
 
@@ -110,7 +107,7 @@ class ViewportCard(object):
                 delta = (event.x - self.foocoords[0], event.y - self.foocoords[1])
             else:
                 delta = (event.x, event.y)
-            self.canvas.move(self.itemid, delta[0], delta[1])
+            self.window.move(delta[0], delta[1])
             self.viewport.reset_scroll_region()
             return "break"
 
@@ -137,57 +134,13 @@ class ViewportCard(object):
         if tkMessageBox.askokcancel("Delete?", "Delete card \"%s\"?" % title_sample):
             # delete card, item, window
             self.card.delete()
-            self.canvas.delete(self.itemid)
             self.window.destroy()
         return "break"
 
-    def frame_mousedown(self, event):
-        # store initial coords and which edges are 
-        self.resize_state = {
-            'start_coords': (event.x, event.y),
-            'last_coords': (event.x, event.y),
-            'left_edge': (0 <= event.x <= self.frame_thickness),
-            'right_edge': (event.x >= self.window.winfo_width() - self.frame_thickness),
-            'top_edge': (0 < event.y < self.frame_thickness),
-            'bottom_edge': (event.y > self.window.winfo_height() - self.frame_thickness)
-        }
-
-    def frame_mousemove(self, event):
-        if self.resize_state:
-            resize = self.resize_state # debug var
-            event_x = event.x
-            event_y = event.y
-            # distance of cursor from original position of window
-            delta = map(int, (event.x - self.resize_state['start_coords'][0],
-                              event.y - self.resize_state['start_coords'][1]))
-            # load current pos, size
-            new_x, new_y = self.canvas_coords()
-            new_width = int(self.canvas.itemcget(self.itemid, 'width'))
-            new_height = int(self.canvas.itemcget(self.itemid, 'height'))
-            # handle x resize/move
-            if self.resize_state['left_edge']:
-                # must move pos and resize
-                new_x += delta[0]
-                new_width -= delta[0]
-            elif self.resize_state['right_edge']:
-                new_width += (event.x - self.resize_state['last_coords'][0])
-            # handle y resize/move
-            if self.resize_state['top_edge']:
-                new_y += delta[1]
-                new_height -= delta[1]
-            elif self.resize_state['bottom_edge']:
-                new_height += (event.y - self.resize_state['last_coords'][1])
-            # save new settings in item, not card yet
-            self.resize_state['last_coords'] = (event.x, event.y)
-            self.canvas.coords(self.itemid, new_x, new_y)
-            self.canvas.itemconfig(self.itemid, width=new_width, height=new_height)
-
-    def frame_mouseup(self, event):
-        if self.resize_state:
-            self.card._x, self.card._y = self.canvas_coords()
-            self.card._w = int(self.canvas.itemcget(self.itemid, 'width'))
-            self.card._h = int(self.canvas.itemcget(self.itemid, 'height'))
-            #print 'saving:', self.card.x, self.card.y, self.card.w, self.card.h
-            self.card.save()
-            self.resize_state = None
+    def save_card(self):
+        # grab values from self.window
+        self.card._x, self.card._y = self.window.canvas_coords()
+        self.card.w, self.card._h = self.window.winfo_width(), self.window.winfo_height()
+        self.card.save()
+        
  
